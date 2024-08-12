3364e961055cc04a36a6408bf4b754a6b6146845
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-08-12 21:58:54 +0100

    Added method to create user

---
+++ backend/api/services/userService.js
@@ -0,0 +1,31 @@
+import UserModel from '../models/userModel.js';
+
+const userModel = new UserModel();
+
+class UserService {
+    static async getUserByEmail(email) {
+        return await userModel.getUserByEmail(email);
+    }
+
+    static async createUser(data) {
+        const { name, email, phone, hashedPasswd, role } = data;
+        const password = hashedPasswd;
+        
+        if (!email) {
+            throw new Error('Email is required');
+        }
+
+        if(!password) {
+            throw new Error('Password is required');
+        }
+
+        if (!phone) {
+            throw new Error('Phone is required');
+        }
+
+        const user  = await userModel.create({ name, email, phone, password, role }); 
+        return user;
+    }
+}
+
+export default UserService;
