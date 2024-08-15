965ac2387ad244f0fbbeaa6a983a184b08ecc7ea
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-08-15 22:59:06 +0000

    refactor: responsive layout

--- frontend/src/pages/ProductListing.jsx
+++ frontend/src/pages/ProductListing.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
-import ProductHeader from '../components/home/ProductHeader'
-import MainProductCard from '../components/common/MainProductCard'
+import React from 'react';
+import ProductHeader from '../components/home/ProductHeader';
+import MainProductCard from '../components/common/MainProductCard';
 
 const ProductListing = () => {
-
-  const dummyData = [1, 2, 3]
+  const dummyData = [1, 2, 3];
 
   return (
-    <section className='min-h-[100vh]'>
-      <ProductHeader heading='trending' description='Top Products' />
-      <div className="flex items-center mt-10 justify-around">
-        {
-          dummyData.map((item) => (
-            <MainProductCard />
-          ))
-        }
+    <section className="min-h-[100vh] p-4">
+      <ProductHeader heading="trending" description="Top Products" />
+      <div className="flex flex-wrap items-center mt-10 justify-center gap-6">
+        {dummyData.map((item, index) => (
+          <MainProductCard key={index} />
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default ProductListing
+export default ProductListing;
--- frontend/src/pages/homepage/ProductCategory.jsx
+++ frontend/src/pages/homepage/ProductCategory.jsx
@@ -1,32 +1,31 @@
-import React from 'react'
-import ProductHeader from '../../components/home/ProductHeader'
-import ProductCard from '../../components/common/ProductCard'
-import SecondCategoryCard from '../../components/common/SecondCategoryCard'
-
+import React from 'react';
+import ProductHeader from '../../components/home/ProductHeader';
+import ProductCard from '../../components/common/ProductCard';
+import SecondCategoryCard from '../../components/common/SecondCategoryCard';
 
 // Dummy data
-const dummyData = [1, 2, 3]
+const dummyData = [1, 2, 3];
 
 const ProductCategory = () => {
     return (
         <div className='min-h-[100vh]'>
-            <ProductHeader heading={"top category"} description={"Explore Categories"} />
-            <div className="flex items-center mt-10 justify-around">
-                {
-                    dummyData.map((data) => (
-                        <ProductCard />
-                    ))
-                }
+            <ProductHeader heading={"Top Category"} description={"Explore Categories"} />
+            
+            {/* Responsive flexbox for ProductCard */}
+            <div className="flex flex-wrap items-center mt-10 justify-around gap-5 m-4">
+                {dummyData.map((data, index) => (
+                    <ProductCard key={index} className="flex-1 min-w-[250px] max-w-[300px]" />
+                ))}
             </div>
-            <div className="flex items-center mt-10 justify-center gap-5">
-                {
-                    dummyData.slice(0, 2).map((data) => (
-                        <SecondCategoryCard />
-                    ))
-                }
+            
+            {/* Responsive flexbox for SecondCategoryCard */}
+            <div className="flex flex-wrap items-center mt-10 justify-center gap-5">
+                {dummyData.slice(0, 2).map((data, index) => (
+                    <SecondCategoryCard key={index} className="flex-1 min-w-[250px] max-w-[300px]" />
+                ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ProductCategory
+export default ProductCategory;
